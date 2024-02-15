ee22f164b26a094bff911266c6c67c0d2d00181a
detlefarend <detlef@arend-privat.net>
2024-02-15 10:10:25 +0100

    corrections on RTD

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -30,8 +30,14 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx_copybutton',
-              'sphinx.ext.autosectionlabel', 'sphinx_multitoc_numbering']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "sphinx.ext.autosectionlabel",
+    "sphinx_multitoc_numbering",
+    "sphinxcontrib.jquery",
+]
 autodoc_member_order = 'bysource'
 
 # Add any paths that contain templates here, relative to this directory.
