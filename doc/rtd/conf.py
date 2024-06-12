82cc5ca0381824dd07750346441431e115bbb9de
steveyuwono <steveyuwono@gmail.com>
2024-06-12 13:11:58 +0200

    Debug: Fixing HowTo Validations #10

--- doc/rtd/conf.py
+++ doc/rtd/conf.py
@@ -22,7 +22,7 @@ copyright = '2024 South Westphalia University of Applied Sciences, Germany'
 author = 'Detlef Arend, Steve Yuwono, Laxmikant Shrikant Baheti et al'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='mlpro-int-sb3',
-version='1.0.0',
+version='1.0.1',
 description='MLPro: Integration StableBaselines3',
 author='MLPro Team',
 author_mail='mlpro@listen.fh-swf.de',
@@ -12,7 +12,7 @@ packages=['mlpro_int_sb3'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "mlpro_int_gymnasium[full]>=1.0.1",
+        "mlpro_int_gymnasium[full]>=1.0.2",
         "mlpro_int_mujoco[full]>=1.0.1",
         "stable_baselines3>=2.3.0"
     ],
