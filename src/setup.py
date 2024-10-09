8388a88397c47324fd1d80a7ac6d5d0ddde3e639
steveyuwono <steveyuwono@gmail.com>
2024-10-09 14:49:11 +0200

    gymnasium<0.30,>=0.28.1

--- src/setup.py
+++ src/setup.py
@@ -12,9 +12,9 @@ packages=['mlpro_int_sb3'],
 # Package dependencies for full installation
 extras_require={
     "full": [
-        "mlpro_int_gymnasium[full]>=1.0.2",
+        "mlpro_int_gymnasium>=1.0.2",
         "mlpro_int_mujoco[full]>=1.0.1",
-        "gymnasium<1.0.0",
+        "gymnasium<0.30,>=0.28.1",
         "stable_baselines3>=2.3.0",
         "torch>=2.0.0,<=2.3.1",
         "numpy>=1.0.0,<=1.26.4"
